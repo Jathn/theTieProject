61ad8f02198ed33c659d31a7d94894f74aed3804
Jathn <jonathan.ramstedt@gmail.com>
2024-03-23 18:10:55 +0200

    Removed legacy from config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
-import legacy from '@vitejs/plugin-legacy';
 
 dotenv.config();
 // https://vitejs.dev/config/
