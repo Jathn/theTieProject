31b06692f460028ed5267cfa4ce165a1bb7e301d
Jathn <jonathan.ramstedt@gmail.com>
2024-03-23 18:04:39 +0200

    updated for deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
+import legacy from '@vitejs/plugin-legacy';
 
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+  ],
+  base: '/theTieProject/',
+});
